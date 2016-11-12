f2fcdee329b8998da9737f684511379e9a9fcc2b
Roman Tischenko <udatos@gmail.com>
2016-11-12 14:39:15 +0200

    Make list of top heroes

--- client/page/Dashboard.elm
+++ client/page/Dashboard.elm
@@ -9,10 +9,7 @@ import Shared.Helpers exposing (hrefClick)
 import String exposing (toLower)
 
 
-
-
-
-        
+       
 
 
 view: List Hero -> Html Msg
@@ -20,21 +17,28 @@ view heroes =
     div [class "container th-min-height"] 
         [ h1 []  [text "Dashboard Page"]
         , ul []
-            (topHeroes heroes)
+            (topHeroes <| bestHeroes heroes)
         ]
 
 topHeroes: List Hero -> List (Html Msg)
 topHeroes =
     List.map 
-        (\h -> 
-            li 
-            []
+        (\h -> li []
             [ a
                 [ hrefClick Navigate <| "/profile/" ++ toLower h.name  
                 , href <| "/profile/" ++ toLower h.name
                 ]
                 [text h.name] 
             ] 
-            
-        ) 
-     
+        )
+         
+bestHeroes : List Hero -> List Hero
+bestHeroes =
+    List.sortWith flippedComparison >> List.take 5
+
+
+flippedComparison a b =
+    case compare a.rating b.rating of
+      LT -> GT
+      EQ -> EQ
+      GT -> LT
