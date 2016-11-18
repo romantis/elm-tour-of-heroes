a911aa6225c037af9eacfc7b3b67046acc5123a1
Roman Tischenko <udatos@gmail.com>
2016-11-18 12:16:35 +0200

    Add helper function  to capitalize string

--- src/Helpers.elm
+++ src/Helpers.elm
@@ -3,6 +3,9 @@ module Helpers exposing (..)
 import Html exposing (Attribute)
 import Html.Events exposing (onWithOptions, defaultOptions)
 import Json.Decode as Json exposing (succeed)
+import String
+
+
 
 hrefClick : (String -> msg) -> String -> Attribute msg
 hrefClick msg url =
@@ -10,3 +13,8 @@ hrefClick msg url =
         "click"  
         {defaultOptions | preventDefault = True }
         (succeed (msg url))
+
+
+capitalize : String -> String
+capitalize s =
+    (String.toUpper <| String.left 1 s) ++ String.dropLeft 1 s
