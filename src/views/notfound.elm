808c85afe329693092a142f887e3e5e25c0252df
Roman Tischenko <udatos@gmail.com>
2016-11-18 13:52:01 +0200

    Simplify and refactor

--- src/views/notfound.elm
+++ src/views/notfound.elm
@@ -10,40 +10,28 @@ import Html.Attributes exposing (class, href, style)
 
 view : Html msg
 view  = 
-    div [class "container th-min-height"] 
-        [ div [ class "" ]
-            [ div [ class "" ]
-                [ div 
-                    [ style
-                        [ "padding" => "2rem"
-                        ,"text-align" => "center"
-                        ]
-                    ]
-                    [ h1 [] [ text "Oops!" ]
-                        
-                    , h2 [] [ text "404 Not Found" ]
-                        
-                    , p [] [ text "Sorry, an error has occured, Requested page not found!" ]
-                    , div 
-                        [ style
-                            [ "margin" => "1rem 0"
-                            ] 
-                        ]
-                        [ a 
-                            [ class ""
-                            , href "/" 
-                            ]
-                            [ span [ class "" ] []
-                            , text "Take Me Home "
-                            ]
-                        , a 
-                            [ class ""
-                            , href "mailto:support@act.com" ]
-                            [ span [ class "glyphicon glyphicon-envelope" ] []
-                            , text "Contact Support "
-                            ]
-                        ]
-                    ]
+    div 
+        [ style
+            [ "padding" => "2em"
+            , "text-align" => "center"
+            ]
+        ]
+        [ h1 [] [ text "Oops!" ]
+        , h2 [] [ text "404 Not Found" ]
+        , p [] [ text "Sorry, an error has occured, Requested page not found!" ]
+        , div 
+            [ style
+                [ "margin" => "1em"
+                ] 
+            ]
+            [ a 
+                [ href "/"
+                , style
+                    ["margin-right" => "1em"]
+                ]
+                [ text "Take Me Home "]
+            , a [ href "mailto:support@example.com" ]
+                [ text "Contact Support "
                 ]
             ]
         ]
